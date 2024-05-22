dfefee5d07251debf3654bc4624b5ec8dc831ef6
Cuyeshi Yunkay <cuyeshi@mail.ru>
2024-05-22 13:05:28 +0300

    Добавьте файлы проекта.

---
+++ LINQtoSQL_Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtoSQL_Library
+{
+    public class Class1
+    {
+    }
+}
